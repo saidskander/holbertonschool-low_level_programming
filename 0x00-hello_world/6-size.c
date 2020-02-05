5ccab26db2af06d680f974f381dbc4d2c6467298
saidskander <askander199h@outlook.com>
2020-02-05 16:46:48 +0100

    size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,10 @@
  */
 int main(void)
 {
-printf(sizeof(5)\n);
+printf("s of char:\n", sizeof(char));
+printf("s of an int:\n", sizeof(int));
+printf("s of long int:\n", sizeof(long));
+printf("s of long int:\n", sizeof(long long));
+printf("s of float:\n", sizeof(float));
 return (0);
 }
