4de6ea731fd862f404483e33e45d75a703280660
saidskander <askander199h@outlook.com>
2020-02-05 19:51:51 +0100

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main - writes text to stdout
- * description: uses write to stdout
+ * main - me forever
+ * description: stdout
  * Return: returns 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
