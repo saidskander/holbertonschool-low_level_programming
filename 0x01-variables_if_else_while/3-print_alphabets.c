8a747431e2243a8f11b7d9f247f6d54cc5ed5353
saidskander <askander199h@outlook.com>
2020-02-06 23:38:45 +0100

    lowercase&uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - detect
+ * Return: 0
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - detect
+ * Return: 0
+ **/
+int main(void)
+{
+char ch1;
+ch1 = 'a';
+while (ch1 <= 'z')
+{
+putchar (ch1);
+ch1 = ch1 + 1;
+}
+ch1 = 'A';
+while (ch1 <= 'Z')
+{
+putchar (ch1);
+ch1 = ch1 + 1;
+}
+putchar ('\n');
+return (0);
+}
