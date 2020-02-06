51ef45f85afe5fc3fe89043cefcd30133a15cd95
saidskander <askander199h@outlook.com>
2020-02-06 23:57:38 +0100

    digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - detect
+ * Return: 0
+ **/
+int nu;
+for (nu = 48; nu <= 57; nu++)
+{
+putchar(nu);
+}
+putchar('\n');
+return (0);
+}
