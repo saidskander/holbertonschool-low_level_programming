92c54861260c03db6a42f42a43d16cc4260dd52f
saidskander <askander199h@outlook.com>
2020-02-07 09:17:46 +0100

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - detect
+ * Return: 0
+ **/
+int main(void)
+{
+char x;
+for (x = 'z'; x <= 'a'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
