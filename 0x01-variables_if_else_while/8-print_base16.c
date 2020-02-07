c3ecbbdf201164da537de22cb93b9cd5dc21f093
saidskander <askander199h@outlook.com>
2020-02-07 12:58:51 +0100

    my code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,11 +7,11 @@ int main(void)
 {
 int x;
 char x1;
-for (x == 48; x == 57; x++)
+for (x = 48; x <= 57; x++)
 {
 putchar(x);
 }
-for (x1 == 'a'; x1 == 'f'; x++)
+for (x1 = 'a'; x1 <= 'f'; x++)
 {
 putchar(x1);
 }
