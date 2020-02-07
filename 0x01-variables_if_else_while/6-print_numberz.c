5bba57f881d8424ae92c9c962e3ed7293c0f4e80
saidskander <askander199h@outlook.com>
2020-02-07 10:18:46 +0100

    my code

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - detect my code
- * Return: 0
- **/
+* main - detect my code
+* Return: 0
+**/
 int main(void)
 {
 int nu;
-for (nu = 48; nu <= 57; nu++)
+for (n = 48; nu <= 57; nu++)
 {
 putchar(nu);
 }
