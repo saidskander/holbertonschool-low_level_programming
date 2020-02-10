3b601e12113c8a5c3a8077a9da16b80248d3cf76
saidskander <askander199h@outlook.com>
2020-02-10 21:30:45 +0100

    dir-0x02

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
 * main - detect my code
 * Return: 0
@@ -13,10 +11,11 @@ for (x + '0'; x <= 99; x++)
 putchar((x / 10) + '0');
 putchar((x % 10) + '0');
 }
-if (x != 99)
+if (x < 99)
 {
 putchar(',');
 putchar(' ');
+x++;
 }
 putchar('\n');
 return (0);
