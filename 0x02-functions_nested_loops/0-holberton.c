5d31a9371777194e0f753fdd0433b80c3bb58f49
saidskander <askander199h@outlook.com>
2020-02-10 22:06:26 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - detect my code
+* Return: 0
+**/
+int main(void)
+{
+_putchar('Holbertonschool');
+_putchar('\n');
+return (0);
+}
