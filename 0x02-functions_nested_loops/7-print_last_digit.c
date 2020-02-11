927c303cb9208e310f02a058ab4979494def6d5d
saidskander <askander199h@outlook.com>
2020-02-11 23:05:55 +0100

    last-digitgit add *

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
+ * print_last_digit - check the code for Holberton School students.
  * @l : Var l
  * Return: last digit l.
  */
 int print_last_digit(int l)
 {
-int  =     
+int lenth = l % 10;
+if (lenth < 0)
+lenth = -lenth;
+_putchar(lenth + '0');
+return (lenth);
 }
