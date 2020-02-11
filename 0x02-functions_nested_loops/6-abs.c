c14e0d4b91aca7925b7ca9225fec6a916d6c5098
saidskander <askander199h@outlook.com>
2020-02-11 22:38:37 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _abs - check the code _abs.
+ * @x : var <3
+ * Return: return x
+ */
+int _abs(int x)
+{
+if (x < 0)
+{
+return (-x);
+}
+else
+return (x);
+}
