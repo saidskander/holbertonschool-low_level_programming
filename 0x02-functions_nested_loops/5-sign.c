7f9671ed6026adc5e07b3c8f087f14277aafaac4
saidskander <askander199h@outlook.com>
2020-02-11 22:09:49 +0100

    signs/operators

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,21 @@
 /**
 * print_sign - print_signs
 * @n : variable input
-*Return: signs
+*Return: signs.
 */
 int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar ('+');
+_putchar('+');
 return (1);
 }
-if (n == 0)
+else if (n == 0)
 {
-_putchar ('0');
+_putchar('0');
 return (0);
 }
-if (n < 0)
-{
-_putchar ('-');
+else if (n < 0)
+_putchar('-');
 return (-1);
-}
 }
