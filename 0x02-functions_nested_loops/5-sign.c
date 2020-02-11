aace9b92554fad8899c23ab1643dbd8515ff678e
saidskander <askander199h@outlook.com>
2020-02-11 21:48:38 +0100

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* print_sign - print_signs
+* @n : variable input
+*Return: signs
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+if (n < 0)
+{
+_putchar ('-');
+return (-1);
+}
+}
