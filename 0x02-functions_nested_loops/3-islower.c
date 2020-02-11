f3db6576ef711ee7312d085a50646a49a7e6a1cf
saidskander <askander199h@outlook.com>
2020-02-11 19:08:19 +0100

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
-* int _islower(int c) - check the code for Holberton School students.
+* _islower - return 1 or 0.
+* @c : variable input
 */
 int _islower(int c)
 {
-if (c >='a' && c <= 'z')
-return (1);    
+if (c >= 'a' && c <= 'z')
+return (1);
 else
 return (0);
 }
