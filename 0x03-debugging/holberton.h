13ac541e6feee77c4f72078a8a3911bc7cde82bd
saidskander <askander199h@outlook.com>
2020-02-11 23:53:52 +0100

    positive or negative

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,8 +1,18 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef holberton_h
+#define holberton_h
 #include <stdio.h>
-
 void positive_or_negative(int i);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
-#endif /* HOLBERTON_H */
+#endif
