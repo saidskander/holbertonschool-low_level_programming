85e79dd17ff71539567e970fa852c3759708cca6
saidskander <askander199h@outlook.com>
2020-02-13 00:17:11 +0100

    11

---
+++ 0x03-debugging/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - check the code for Holberton School students.
+ * @n : var
+ * Return: Always 0.
+ */
+void print_to_98(int n);
+{
+int x;
+if (n < 98)
+{
+for (x = n; x < 98; x++)
+printf("%d, ", x);
+}
+else if (n > 98)
+{
+for (x = n; x > 98; x--)
+printf("%d, ", x);
+}
+printf("98\n");
+}
+}
