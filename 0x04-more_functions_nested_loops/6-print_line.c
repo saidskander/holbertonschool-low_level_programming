f06477ba5d2c26fbc0002155f0adb207a4c68726
saidskander <askander199h@outlook.com>
2020-02-13 21:50:13 +0100

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n : input
+ * Return:_.
+ */
+void print_line(int n)
+{
+int x;
+for (x = 0; x < n; x++)
+_putchar('_');
+_putchar('\_');
+}
