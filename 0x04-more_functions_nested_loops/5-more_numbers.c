91b695ae5c50ac0d7ca37ca04cf044b3fe2aa71e
saidskander <askander199h@outlook.com>
2020-02-13 21:23:56 +0100

    10Num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: .
+ */
+void more_numbers(void)
+{
+int x;
+int y;
+for (y = 0; y < 10; y++)
+{
+for (x = 0; x < 15; x++)
+{
+if (x > 9)
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+}
+_putchar('\n');
+}
+}
