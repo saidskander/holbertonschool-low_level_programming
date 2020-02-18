5ad071807809daf7f576df2ae389e84bf25b7859
saidskander <askander199h@outlook.com>
2020-02-18 22:04:52 +0100

    ix=2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * puts2 - check the code for Holberton School students.
+ * @str : input .
+ * Return: .
+ */
+void puts2(char *str)
+{
+int x;
+for (x = 0; str[x] != '\0'; x+=2)
+{
+_putchar(str[x]);
+}
+_putchar('\n');
+}
