e78a96a91f43ef60755a58b34d20ac5293e038e4
saidskander <askander199h@outlook.com>
2020-02-18 07:44:37 +0100

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n : input variable
+ * Return: .
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
