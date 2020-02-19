ca9c647a686ec3032f047cb010d48d8a40d30875
saidskander <askander199h@outlook.com>
2020-02-19 23:31:17 +0100

    fixed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,10 @@ void print_array(int *a, int n)
 int x;
 for (x = 0; x < n; x++)
 {
-if (x == n - 1)
-printf("%d\n", a[x]);
+if (x != n - 1)
+printf("%d", a[x]);
 else
 printf("%d, ", a[x]);
 }
+printf("\n");
 }
