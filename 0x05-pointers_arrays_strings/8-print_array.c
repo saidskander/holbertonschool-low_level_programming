99cd0955e1b537236c69626ba52e976fdb14da96
saidskander <askander199h@outlook.com>
2020-02-19 23:36:50 +0100

    fixed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_array - check the code for Holberton School students.
  * @a : input
@@ -12,9 +11,9 @@ int x;
 for (x = 0; x < n; x++)
 {
 if (x != n - 1)
-printf("%d", a[x]);
-else
 printf("%d, ", a[x]);
+else
+printf("%d", a[x]);
 }
 printf("\n");
 }
