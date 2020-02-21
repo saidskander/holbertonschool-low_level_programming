beda1347d35c8cf6223ea41a12a68c6bf38f6516
saidskander <askander199h@outlook.com>
2020-02-22 00:27:58 +0100

    rev array num

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,16 +4,18 @@
  * print_array - check the code for Holberton School students.
  * @a: an array of integers
  * @n: the number of elements to swap
- *
- * Return: nothing.
+ * Return: reverse of the array number.
  */
 void print_array(int *a, int n)
 {
 int x;
 int i;
 n = n - 1;
-for (i = 0; i <= n; i++)
+for (i = 0; i <= n ; i++)
+{
+x = a[i];
 a[i] = a[n];
+a[n] = x;
 n--;
 }
 }
