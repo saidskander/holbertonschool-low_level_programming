b56014a3d23adf03cb45cc86a079b6ee58602b55
saidskander <askander199h@outlook.com>
2020-02-24 21:37:35 +0100

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_string - check the code for Holberton School students.
+ * @s : input **s pointer to a pointer
+ * @to : input
+ * Return: .
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
