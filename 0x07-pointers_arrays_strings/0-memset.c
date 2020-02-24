05de36363bfc76f077cf1464989551552d3692a4
saidskander <askander199h@outlook.com>
2020-02-24 19:55:54 +0100

    memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
+ * *_memset - prints buffer in hexa
+ * @s: input var
+ * @b: input var
+ * @n: the unsigned int
  * Return: Nothing.
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int x;
+for (x = 0; x < n; x++)
+{
+s[x] = b;
+}
+return (s);
+}
