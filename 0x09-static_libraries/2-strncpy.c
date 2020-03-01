2866427acdfb3977d07f7ef837455166adfdf3d9
saidskander <askander199h@outlook.com>
2020-03-01 19:10:57 +0100

    Lib Func

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -4,9 +4,9 @@
  * *_strncpy - prints buffer in hexa
  * @n : input variable
  * @dest : input
- * @src : input 
+ * @src : input
  * Return: dest.
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 }
