b84554e07a4faa9be486fba45ee5a3a2d6070278
saidskander <askander199h@outlook.com>
2020-03-01 23:29:15 +0100

    fixed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - check for holberton-school students
+ *@argv: input
+ *@argc: input
+ *Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
