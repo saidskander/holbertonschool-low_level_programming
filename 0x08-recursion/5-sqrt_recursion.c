228efc45c08d8241a037e2da5280fc62ae81e02c
saidskander <askander199h@outlook.com>
2020-03-01 21:28:31 +0100

    fixed

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * is_prime_number - check the code for Holberton School students.
+ * @n : input ..
+ * Return: .
+ */
+int is_prime_number(int n)
+{
+  x = 0;
+  for (x = 0; i < n; x++)
+    if (x =1)
+
+
+
+    }
