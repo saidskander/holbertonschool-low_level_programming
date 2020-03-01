b1322333e770dafb534e6bcf4e0d778d0ad2ab50
saidskander <askander199h@outlook.com>
2020-03-01 19:08:01 +0100

    Lib Func

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * *_strncpy - prints buffer in hexa
+ * @n : input variable
+ * @dest : input
+ * @src : input 
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+}
