fd655e92b5dd57ff4f7f6b7fe18c6459d4f78f54
saidskander <askander199h@outlook.com>
2020-03-09 23:52:18 +0100

    gg

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog -  variable function of type initdog
+ * @d : pointer to struct dog
+ * @name : input pointer
+ * @age : input float
+ * @owner : input string pointer
+ */
+void init_dog(struct dog *d, float age, char *name, char *owner)
+{
+if (d != NULL)
+{
+x->name = name;
+x->owner = owner;
+x->age = age;
+}
+}
