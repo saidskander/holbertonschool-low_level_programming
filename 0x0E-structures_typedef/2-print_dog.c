d36d75b37706de902c8d455f4ac6689c577836ab
saidskander <askander199h@outlook.com>
2020-03-10 23:34:57 +0100

    care on uppercase

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-if (d->name == NULL && d->Owner == NULL)
+if (d->name == NULL && d->owner == NULL)
 printf("Name: (nil)");
 else
 printf("Name: %s\n", d->name);
