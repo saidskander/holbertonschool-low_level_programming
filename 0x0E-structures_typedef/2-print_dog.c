1ce8db1631cda8537074c9c99d13cf24d55ea881
saidskander <askander199h@outlook.com>
2020-03-10 23:29:47 +0100

    hmm

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ if (d->name == NULL && d->owner == NULL)
 printf("Name: (nil)");
 else
 printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
+printf("Age: %.6f\n", d->age);
 printf("Owner: %s\n", d->owner);
 }
