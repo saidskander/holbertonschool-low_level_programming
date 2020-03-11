55417c937ecdefdecc269ef6fe046dd5f2485cb3
saidskander <askander199h@outlook.com>
2020-03-11 16:03:07 +0100

    Django

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,13 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-if (d->name == NULL && d->owner == NULL)
-{
+if (d->name == NULL)
 printf("Name: (nil)\n");
-printf("Owner: (nil)\n");
-}
 else
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
+if (d->owner== NULL)
+printf("Owner: %s\n", d->name);
+else
 printf("Owner: %s\n", d->owner);
 }
