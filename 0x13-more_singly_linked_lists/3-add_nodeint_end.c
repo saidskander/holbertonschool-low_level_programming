b8b7ae625a59b212e56fdeff1a33a46e37517426
saidskander <askander199h@outlook.com>
2020-03-24 22:08:44 +0100

    done

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * *add_nodeint_end - *add_nodeint_end
+ * @head : listint_t **head
+ * @n : const int n
+ * Return: add_last
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *add_last,*x;
+x = *head;
+add_last = malloc(sizeof(listint_t));
+if (add_last == NULL)
+return (NULL);
+add_last->n = n;
+add_last->next = NULL;
+if (*head == NULL)
+{
+*head = add_last;
+return (add_last);
+}
+for (; x->next; x++)
+;
+x->next = add_last;
+return (add_last);
+}
