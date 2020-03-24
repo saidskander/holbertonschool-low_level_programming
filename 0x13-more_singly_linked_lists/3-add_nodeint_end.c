072ef1cfce85d477b055aebc5a3b02bbdba5d5ad
saidskander <askander199h@outlook.com>
2020-03-24 22:10:11 +0100

    done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *add_last,*x;
+listint_t *add_last, *x;
 x = *head;
 add_last = malloc(sizeof(listint_t));
 if (add_last == NULL)
