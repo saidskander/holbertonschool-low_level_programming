e2154bcc31c09196895383607cffc662cf1fcc08
saidskander <askander199h@outlook.com>
2020-03-24 20:51:18 +0100

    done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_listint -  print_listint
+ * listint_len -  listint_len
  * @h : const listint_t *h
  * Return: n
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 int n = 0;
 for (; h; n++)
 {
-printf("%d\n", h->n);
+n++;
 h = h->next;
 }
 return (n);
