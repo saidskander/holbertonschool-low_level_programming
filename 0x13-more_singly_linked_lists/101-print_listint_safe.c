fb16f6fcd803a8e921ee2c249860650171503908
saidskander <askander199h@outlook.com>
2020-03-26 23:50:29 +0100

    x_x

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint_safe - print_listint_safe
+ * @head: const listint_t *head
+ * Return: 0
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+int x = 0;
+for (; head; x++)
+;
+if (head == NULL)
+exit(98);
+else
+return (0);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,6 +32,8 @@ int pop_listint(listint_t **head);
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
+size_t print_listint_safe(const listint_t *head);
+
 int sum_listint(listint_t *head);
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
