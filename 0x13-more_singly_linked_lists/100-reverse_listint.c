ce33634705c4b5e244ae8f96afd207acc16f63d8
saidskander <askander199h@outlook.com>
2020-03-26 22:56:03 +0100

    easy adv task

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * reverse_listint - reverse_listint
+  * @head: listint_t **head
+  * Return: reverse like a miror <3
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+head++;
+return (0);
+}
