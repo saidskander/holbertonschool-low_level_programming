550fe8126e8554e45fe92878d55e29a1a2832c67
saidskander <askander199h@outlook.com>
2020-03-28 23:59:12 +0100

    omG my files

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * first - first
+ * Return: nothing
  */
-int main(void)
+void __attribute__((constructor)) first()
 {
-printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-return (0);
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
