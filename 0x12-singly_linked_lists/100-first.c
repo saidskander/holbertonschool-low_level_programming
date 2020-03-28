5c3459b4bc011f16d29ffe2f5e56ebce8e36526c
saidskander <askander199h@outlook.com>
2020-03-29 00:02:22 +0100

    omG my files

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
  * first - first
  * Return: nothing
  */
 void __attribute__((constructor)) first()
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow
+,\nI bore my house upon my back!\n");
 }
