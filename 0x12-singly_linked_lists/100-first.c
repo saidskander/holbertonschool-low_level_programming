e050d058d621ba59124c82d54f5672b438ed3d20
saidskander <askander199h@outlook.com>
2020-03-28 21:43:40 +0100

    Done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * TheZoo - TheZoo
+ * Return: void
+ */
+void TheZoo(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
