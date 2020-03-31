03146ddf765786bd0f1e5106b30346ae99638c92
saidskander <askander199h@outlook.com>
2020-03-31 19:49:43 +0100

    Done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "holberton.h"
+#include <fcntl.h>
+/**
+*read_textfile - read_textfile
+*@filename: const char *filename
+*@letters: size_t letters
+*Return: wt
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int rd;
+int wt;
+int x;
+char *space;
+if (filename == NULL)
+return (0);
+x = open(filename, O_RDONLY);
+if (x == -1)
+return (0);
+space = malloc(sizeof(char) * letters);
+if (space == NULL)
+{
+free(space);
+return (0);
+}
+rd = read(x, space, letters);
+if (rd == -1)
+{
+return (0);
+}
+wt = write(STDOUT_FILENO, space ,rd);
+if (wt == -1)
+return (0);
+close(x);
+return (wt = write(STDOUT_FILENO, space ,rd));
+}
