b6e5832af2f1709c211cc04f1a0a70f32465fb3d
saidskander <askander199h@outlook.com>
2020-04-11 15:10:48 +0100

    Done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,29 +2,38 @@
 #include <stdio.h>
 /**
  * cap_string - cap_string
- * @a: character string to capitalize
- * Return: string
+ * @a : char *a
+ * Return: str
  */
 char *cap_string(char *a)
 {
-int newword = 1;
-char *ptr = a;
-while (*ptr != 0)
+int word = 1;
+char *pra;
+for (pra = a; *pra; pra++)
 {
-if (newword == 1)
+if (word == 1)
 {
-newword = 0;
-if (*ptr < 'z' && *ptr > 'a')
-*ptr -= 32;
+word = 0;
+if (*pra < 'z' && *pra > 'a')
+*pra -= 32;
 }
 if
 (
-*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ','
-|| *pr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '"'
-|| *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}'
+*pra == ' '
+|| *pra == '\n'
+|| *pra == '\t'
+|| *pra == ','
+|| *pra == ';'
+|| *pra == '.'
+|| *pra == '!'
+|| *pra == '"'
+|| *pra == '('
+|| *pra == ')'
+|| *pra == '{'
+|| *pra == '}'
 )
-newword = 1;
-ptr++;
+word = 1;
+pra++;
 }
 return (a);
 }
