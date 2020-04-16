6139e211d1779afb715a2d222852702f233dc94f
saidskander <askander199h@outlook.com>
2020-04-16 12:47:48 +0100

    Done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+int j;
+int k;
+for (x = '0'; x < 56; x++)
+{
+for (j = x + 1; j < 57; j++)
+{
+for (k = j + 1; k < 58; k++)
+{
+putchar(x);
+putchar(j);
+putchar(k);
+if (x!= 55 || j != 56 || k != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
