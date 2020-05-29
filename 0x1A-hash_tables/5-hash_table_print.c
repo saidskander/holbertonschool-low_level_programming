2e599ad9f68a798689b74c7913a3e38e11e08994
saidskander <askander199h@outlook.com>
2020-05-29 20:40:38 +0100

    Done

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_print - func prints hash table.
+ * @ht : const hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned int x = 0;
+int begin = 0;
+if (!ht)
+return;
+printf("{");
+for (; x <= ht->size; x++)
+{
+if (ht->array[x])
+{
+for (ht->array[x])
+{
+if (begin != 0)
+printf(", ");
+printf("'%s': '%s'", ht->array[x]->key, ht->array[x]->value);
+begin = 1;
+ht->array[x] = ht->array[x]->next;
+}
+}
+}
+printf("}\n");
+}
