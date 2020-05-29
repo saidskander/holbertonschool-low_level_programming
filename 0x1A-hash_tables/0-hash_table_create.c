da4180e0dc2a1fecb54d6f0611289379c2547a5a
saidskander <askander199h@outlook.com>
2020-05-29 16:41:37 +0100

    Done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,15 +7,9 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *htc;
-unsigned long int x = 0;
 htc = malloc(sizeof(hash_table_t));
 if (htc == NULL)
 return (NULL);
-htc->array = malloc(sizeof(hash_node_t *) * size);
-if (htc->array == NULL)
-return (NULL);
-for (; x < size; x++)
-htc->array[x] = NULL;
-htc->size = size;
+htc->array = malloc(sizeof(htc->array)* size);
 return (htc);
 }
